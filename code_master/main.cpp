0f122b865bb01fdefcaf1d4c45b1d3126e4cb2f7
chulmin12 <gogurye97@gmail.com>
2023-08-13 10:53:35 +0900

    Initial Commit

---
+++ code_master/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  code_master
+//
+//  Created by Hwangchulmin on 2023/08/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
