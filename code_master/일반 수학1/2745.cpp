48f38c2caf59beaff91a47b38389cd5785cc7a24
chulmin12 <gogurye97@gmail.com>
2023-09-07 22:25:40 +0900

    2023.09.07

---
+++ code_master/일반 수학1/2745.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    int b;
+    cin >> str >> b;
+    
+    int b_num;
+    for (int i = 0; i < str.length(); i++){
+        int temp = str[str.length() - (i + 1)];
+    }
+    
+}
