864125637bde1e36538b7667963b63f8f36b9cd1
chulmin12 <gogurye97@gmail.com>
2023-09-18 20:53:51 +0900

    2023.09.18

--- code_master/브루트 포스/2798.cpp
+++ code_master/브루트 포스/2798.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
+    int N, M;
+    cin >> N >> M;
+    
+    vector<int> cards;
+    
+    for (int i = 0; i < N; i++){
+        int a;
+        cin >> a;
+        cards.push_back(a);
+    }
+    
+    sort(cards.begin(),cards.end());
+    
+    int sum = 0;
+    int i = N - 1;
+    while(true){
+        sum = cards[i] + cards[i - 1] + cards[i - 2];
+        if (sum > M){
+            i--;
+        }
+        else{
+            cout << sum << endl;
+            break;
+        }
+    }
     
 }
