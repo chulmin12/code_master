1f88ed5e5250123a99c09fa96816caaee79db7f5
chulmin12 <gogurye97@gmail.com>
2023-09-24 19:02:05 +0900

    2023.09.24 체스판 어렵네...

--- code_master/브루트 포스/1018.cpp
+++ code_master/브루트 포스/1018.cpp
@@ -1,27 +1,77 @@
 #include <iostream>
 #include <vector>
-// 여기부터 다시 하기
+// INT_MAX를 사용하기 위한 라이브러리
+#include <limits.h>
+
 using namespace std;
 
-int main()
-{
+// 체스판 초기화
+string WB[8] = {
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW"
+};
+
+string BW[8] = {
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB"
+};
+
+int wb_first(int x, int y, char arr[][51]) {
+    int cnt = 0;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (arr[x + i][y + j] != WB[i][j])
+                cnt++;
+        }
+    }
+    return cnt;
+}
+
+int bw_first(int x, int y, char arr[][51]) {
+    int cnt = 0;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (arr[x + i][y + j] != BW[i][j])
+                cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    // 체스판 입력부분,,
     int n, m;
     cin >> n >> m;
-    char arr[n+1][m+1];
-    
+    char arr[51][51];
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cin >> arr[i][j];
         }
     }
-    
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << arr[i][j] << " ";  // Add a space after each character
+    // 최대로 해놓고 값보다 작을 경우 업데이트 하기 위해,,
+    int minCnt = INT_MAX;
+
+    for (int i = 0; i <= n - 8; i++) {
+        for (int j = 0; j <= m - 8; j++) {
+            int cnt = min(wb_first(i, j, arr), bw_first(i, j, arr));
+            minCnt = min(minCnt, cnt);
         }
-        cout << endl;
     }
-    
+
+    cout << minCnt << '\n';
+
     return 0;
 }
-
